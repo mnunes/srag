e643e5031754b80301c50ec3038a94be2c0cdc9f
Marcus Nunes <marcus.nunes@gmail.com>
2020-04-19 18:48:06 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ pageWithSidebar(
     p("Infelizmente, não há testes de COVID-19 para todos os suspeitos. A", strong("Síndrome Respiratória Aguda Grave (SRAG)"), "possui sintomas muito parecidos com aqueles do COVID-19. Acredito que, com o avanço da pandemia, é importante que a população tenha noção da quantidade de casos da doença que estão por aí."),
     p("Entretanto, temos um bom acesso a casos registrados de SRAG. A Fiocruz tem uma excelente ferramenta de divulgação chamada", a("Info Gripe", href="http://info.gripe.fiocruz.br/", target="_blank"), ", no qual obtive os dados aqui divulgados.",),
     p(strong("ATENÇÃO:"), "alguns gráficos podem dar a entender que está acontecendo um achatamento na curva. É preciso analisar com cuidado, pois um número menor de dados em semanas mais recentes pode significar atraso na notificação.",),
-    p("Aplicativo desenvolvido por", a("Marcus Nunes", href="https://marcusnunes.me/", target="_blank"), ". O código está disponível", a("no github", href="https://github.com/mnunes/subnotificacao/", target="_blank"), ".")
+    p("Aplicativo desenvolvido por", a("Marcus Nunes", href="https://marcusnunes.me/", target="_blank"), ". O código está disponível", a("no github", href="https://github.com/mnunes/srag/", target="_blank"), ".")
   ),
   
   mainPanel(
